b87d5963a655f8ff4e1bfac1e716679ddfe9614b
Josh Wang <josh.wang@lmigroup.com>
2020-10-13 23:57:07 +1100

    feat: add native button props for the button component

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Button disabled>Hello</Button>
-        <Button>Hello</Button>
+        <Button onClick={() => { alert("hahaha") }} className='customer'>Hello</Button>
         <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Primary Large</Button>
         <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>Danger Small</Button>
-        <Button btnType={ButtonType.Link} href='https://www.google.com'> Google</Button>
+        <Button btnType={ButtonType.Link} href='https://www.google.com' target='_blank'> Google</Button>
         <Button btnType={ButtonType.Link} href='https://www.google.com' disabled> Google</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
