2bce8272a10e6755eb0d342aa19e51ccfa7ed692
Josh Wang <josh.wang@lmigroup.com>
2020-10-13 21:50:09 +1100

    feat: add button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import React from "react";
+import Button, { ButtonType, ButtonSize } from './components/Button/button'
 
 function App() {
- 
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello world</h1>
-        <h2>Hello world</h2>
-        <h3>Hello world</h3>
-        <hr/>
-        <code>
-          const a = 'b'
-        </code>
+        <Button disabled>Hello</Button>
+        <Button>Hello</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Hello</Button>
+        <Button btnType={ButtonType.Link} href='https://www.google.com'> Google</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
